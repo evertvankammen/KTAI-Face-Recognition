e3f7ccb8d1c30df66764561254032b6eb629d52a
evert <evert.vankammen@qfactors.nl>
2024-03-01 17:47:57 +0100

    Move face detection code to experiment.py

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -2,6 +2,9 @@ from typing import Tuple, Union
 import math
 import cv2
 import numpy as np
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 MARGIN = 10  # pixels
 ROW_SIZE = 10  # pixels
@@ -71,6 +74,28 @@ def visualize(
     return annotated_image
 
 
+IMAGE_FILE = '..\\data\\pictures\\img.png'
+
+img = cv2.imread(IMAGE_FILE)
+cv2.imshow("image", img)
+
+# STEP 1: Import the necessary modules.
 
 
+# STEP 2: Create an FaceDetector object.
+base_options = python.BaseOptions(model_asset_path='../models/detector.tflite')
+options = vision.FaceDetectorOptions(base_options=base_options)
+detector = vision.FaceDetector.create_from_options(options)
 
+# STEP 3: Load the input image.
+image = mp.Image.create_from_file(IMAGE_FILE)
+
+# STEP 4: Detect faces in the input image.
+detection_result = detector.detect(image)
+
+# STEP 5: Process the detection result. In this case, visualize it.
+image_copy = np.copy(image.numpy_view())
+annotated_image = visualize(image_copy, detection_result)
+rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+cv2.imshow("rotated", rgb_annotated_image)
+cv2.waitKey(0)
