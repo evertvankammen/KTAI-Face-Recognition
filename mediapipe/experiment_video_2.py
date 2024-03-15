8b8aeb3a3ac13616598e6af007cc99a3f7d2c32f
evkammen <evert@evkammen.nl>
2024-03-15 15:32:49 +0100

    Extend face detection functionality in experiment_video_2.py
    Added additional features to the script experiment_video_2.py. Now, it calculates the euclidean distance between faces, identifies the most common face in a certain area and opens a GUI for that specific frequent face. Imported additional modules, including math and collections.Counter, for these functionalities.

--- mediapipe/experiment_video_2.py
+++ mediapipe/experiment_video_2.py
@@ -1,4 +1,6 @@
+import math
 import os
+from collections import Counter
 
 import cv2
 import mediapipe as mp
@@ -6,6 +8,7 @@ import numpy as np
 from mediapipe.tasks import python
 from common import visualize
 from picture_compare import SimpleFacerec
+import tkinter as tk
 import userinteraction.userinterface as gui
 
 
@@ -25,6 +28,10 @@ sfc = SimpleFacerec()
 image_path = os.path.join("..", "data", "pictures")
 #sfc.save_encodings_images(image_path)
 sfc.read_encoded_images()
+
+faces_in_box = []
+
+
 with FaceDetector.create_from_options(options) as detector:
     cap = cv2.VideoCapture(VIDEO_FILE)
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -37,26 +44,47 @@ with FaceDetector.create_from_options(options) as detector:
 
     frame_counter = 0
     frame_duration_counter = 0
+    previous_x, previous_y = 0, 0
     # Read until video is completed
     while cap.isOpened():
         # Capture frame-by-frame
         ret, frame = cap.read()
+
+
         if ret:
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
             mp_image_temp = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             face_detector_result = detector.detect_for_video(mp_image_temp, round(frame_duration_counter))
-
             for result in face_detector_result.detections:
+                distance = math.sqrt((previous_x - result.bounding_box.origin_x)**2  + (previous_y - result.bounding_box.origin_y)**2)
                 if result.categories[0].score > 0.75:
                     result.categories[0].category_name = sfc.face_lowest_distances(result.keypoints)
+                else:
+                    result.categories[0].category_name = 'xxx'
+
+                if distance < 100:
+                    print('same box')
+                    if result.categories[0].category_name != 'xxx':
+                        faces_in_box.append(result.categories[0].category_name)
+                else:
+                    print('other box')
+                    print(faces_in_box)
+                    if len(faces_in_box) > 0:
+                        counts = Counter(faces_in_box)
+                        most_frequent, the_count = counts.most_common(1)[0]
+                        print(most_frequent, the_count)
+                        gui.start_gui(most_frequent)
+                    faces_in_box = []
+
+                previous_x, previous_y = result.bounding_box.origin_x, result.bounding_box.origin_y
 
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
             image_copy = np.copy(mp_image.numpy_view())
             annotated_image = visualize(image_copy, face_detector_result)
             cv2.imshow("Annotated", annotated_image)
-            if frame_counter % round(fps) == 0 and 1 == 2:
+            if frame_counter % round(fps) == 0 and 1==2:
                 for result in face_detector_result.detections:
                     if result.categories[0].score > 0.75:
                         gui.start_gui(result.categories[0].category_name)
