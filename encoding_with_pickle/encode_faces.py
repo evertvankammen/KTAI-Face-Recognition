6f71d1db0e1b6b4c468f99bef2b129c7c7ccb144
Ralph Depondt <rr.depondt@gmail.com>
2024-03-22 15:05:20 +0100

    Veel meer gezichten toegevoegd en een manier om dit op te slaan

--- encoding_with_pickle/encode_faces.py
+++ encoding_with_pickle/encode_faces.py
@@ -18,7 +18,8 @@ imagePaths = get_image_files("dataset")
 # initialize the list of known encodings and known names
 knownEncodings = []
 knownNames = []
-
+# Track the number of images processed for each person
+processed_images_per_person = {}
 # loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
     # extract the person name from the image path
@@ -26,26 +27,31 @@ for (i, imagePath) in enumerate(imagePaths):
                                                  len(imagePaths)))
     # Getting the name of the actor from the directory: dataset/<Actor>?/..
     name = imagePath.split(os.path.sep)[-2]
-    # load the input image and convert it from BGR (OpenCV ordering)
-    # to dlib ordering (RGB)
-    image = cv2.imread(imagePath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # If the person's images processed are less than 3, proceed with processing
+    if name not in processed_images_per_person:
+        processed_images_per_person[name] = 0
+    if processed_images_per_person[name] < 3:
+        # load the input image and convert it from BGR (OpenCV ordering)
+        # to dlib ordering (RGB)
+        image = cv2.imread(imagePath)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # detect the (x, y)-coordinates of the bounding boxes
-    # corresponding to each face in the input image
-    boxes = face_recognition.face_locations(rgb, model='hog')
-    # compute the facial embedding for the face
-    encodings = face_recognition.face_encodings(rgb, boxes)
-    # loop over the encodings
-    for encoding in encodings:
-        # add each encoding + name to our set of known names and
-        # encodings
-        knownEncodings.append(encoding)
-        knownNames.append(name)
+        # detect the (x, y)-coordinates of the bounding boxes
+        # corresponding to each face in the input image
+        boxes = face_recognition.face_locations(rgb, model='cnn')
+        # compute the facial embedding for the face
+        encodings = face_recognition.face_encodings(rgb, boxes)
+        # loop over the encodings
+        for encoding in encodings:
+            # add each encoding + name to our set of known names and
+            # encodings
+            knownEncodings.append(encoding)
+            knownNames.append(name)
+        processed_images_per_person[name] += 1
 
 # dump the facial encodings + names to disk
 print("[INFO] serializing encodings...")
 data = {"encodings": knownEncodings, "names": knownNames}
-f = open("encodings.pickle", "wb")
+f = open("encodings_cnn.pickle", "wb")
 f.write(pickle.dumps(data))
 f.close()
--- encoding_with_pickle/recognize_faces_video.py
+++ encoding_with_pickle/recognize_faces_video.py
@@ -1,37 +1,55 @@
 # import the necessary packages
-from imutils.video import VideoStream
 import face_recognition
-import imutils
 import pickle
 import time
 import cv2
 import os
 
 
+# Initialisaties
 video_path = os.path.join("..", "data", "pictures", "Friends.mp4")
-output_path= os.path.join("output_result_videos","test3.avi")
-show_display= 1
+output_path = os.path.join("output_result_videos", "test3.avi")
+show_display = 1
 
 # load the known faces and embeddings
 print("[INFO] loading encodings...")
-data = pickle.loads(open('encodings.pickle', "rb").read())
+data = pickle.loads(open('encodings_cnn.pickle', "rb").read())
 # initialize the video stream and pointer to output video file, then
 # allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 vs = cv2.VideoCapture(video_path)
+vs.set(cv2.CAP_PROP_FPS, 30)
 
 writer = None
 time.sleep(2.0)
 
+
+def resize_frame(shape, desired_width):
+    # convert the input frame from BGR to RGB then resize it to have
+    # a smaller width (to speedup processing)
+    height, width, _ = shape  # Get original image dimensions
+    new_height = int(height * (desired_width / width))  # Calculate proportional height
+    return desired_width, new_height
+
 # loop over frames from the video file stream
+def name_box(cv2, frame, left, top, right, bottom, name):
+    # draw the predicted face name on the image
+    cv2.rectangle(frame, (left, top), (right, bottom),
+                  (0, 255, 0), 2)
+    y = top - 15 if top - 15 > 15 else top + 15
+    cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+                0.75, (0, 255, 0), 2)
+
+
 while vs.isOpened():
     # grab the frame from the threaded video stream
     _, frame = vs.read()
 
-    # convert the input frame from BGR to RGB then resize it to have
-    # a width of 750px (to speedup processing)
+    new_size = resize_frame(frame.shape, 450)
+
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    rgb = imutils.resize(frame, width=750)
+    rgb = cv2.resize(frame, dsize=new_size, interpolation=cv2.INTER_CUBIC)
+
     r = frame.shape[1] / float(rgb.shape[1])
     # detect the (x, y)-coordinates of the bounding boxes
     # corresponding to each face in the input frame, then compute
@@ -46,7 +64,7 @@ while vs.isOpened():
         # attempt to match each face in the input image to our known
         # encodings
         matches = face_recognition.compare_faces(data["encodings"],
-                                                 encoding, tolerance=0.5)
+                                                 encoding, tolerance=0.6)
         name = "Unknown"
         # check to see if we have found a match
         if True in matches:
@@ -75,12 +93,9 @@ while vs.isOpened():
             right = int(right * r)
             bottom = int(bottom * r)
             left = int(left * r)
-            # draw the predicted face name on the image
-            cv2.rectangle(frame, (left, top), (right, bottom),
-                          (0, 255, 0), 2)
-            y = top - 15 if top - 15 > 15 else top + 15
-            cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.75, (0, 255, 0), 2)
+
+            name_box(cv2, frame, left, top, right, bottom, name)
+
 
         # if the video writer is None *AND* we are supposed to write
         # the output video to disk initialize the writer
