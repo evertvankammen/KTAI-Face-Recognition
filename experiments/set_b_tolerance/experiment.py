9a641de1ebc7275c2f87f2ca8c417fe2e19bc5d8
evert <evert.vankammen@qfactors.nl>
2024-04-15 22:58:04 +0200

    Add graph functions and update experiment script

--- experiments/set_b_tolerance/experiment.py
+++ experiments/set_b_tolerance/experiment.py
@@ -1,8 +1,6 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 from matplotlib import pyplot as plt
-
-from experiments.shared.shared import experiment
 
 
 def calculate_mse(c1: Counter, c1frames: int, c2: Counter, c2frames: int):
@@ -33,13 +31,49 @@ def plot_actor_frequencies(file_path, tolerance, multiplier=1.0):
     plt.bar(actors, frequencies, color=colors)
     addlabels(actors, frequencies)
     plt.xlabel('Actors')
-    plt.ylabel('Frequency %')
+    plt.ylabel('Frequency')
     plt.title(f'Frequency of Recognized Actors Over Video Frames\ntaken from Friends tolerance: {tolerance}')
     plt.xticks(rotation=45)
     plt.ylim([0, 5500])  # Set y-axis limit slightly above the maximum frequency
     store_experiment = f"frequency_tolerance_{tolerance}.png"
     plt.savefig(store_experiment)  # save the figure to file
 
+    plt.show()
+
+
+def plot_video_frames(file_path, tolerance):
+    """Plot frames where actors are recognized from a text file."""
+    actor_frames = defaultdict(list)
+
+    with open(file_path, 'r') as fp:
+        lines = fp.readlines()
+        for line in lines[4:]:
+            actor, frame = line.split()
+            actor_frames[actor].append(int(frame))
+
+    s = sorted(actor_frames.items())
+
+    # Voeg "Unknown" toe als sleutel in het geval het niet in de dataset voorkomt
+    if "Unknown" not in actor_frames:
+        actor_frames["Unknown"] = []
+
+    plt.figure(figsize=(10, 6))
+    colors = ['blue', 'green', 'red', 'purple', 'orange', 'brown', 'gray']
+
+    for i, (actor, frames) in enumerate(s):
+        # Plot elk punt met een specifieke kleur
+        plt.plot([actor] * len(frames), frames, marker='o', linestyle='None', label=actor,
+                 color=colors[i % len(colors)])
+
+    plt.xlabel('Actors')
+    plt.ylabel('Frame Number')
+    plt.title(f'Frames where Actors are Recognized\nTolerance: {tolerance}')
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.grid(True)
+
+    store_experiment = f"actor_frames_tolerance_{tolerance}_sample_probability_0.25.png"
+    plt.savefig(store_experiment)  # save the figure to file
     plt.show()
 
 
@@ -55,11 +89,14 @@ if __name__ == '__main__':
     # experiment(nr_of_processes=32, desired_tolerance=0.70, model='cnn')
 
     # create_mse()
-    plot_actor_frequencies("exp_results_t_0.5_m_hog_u_1.txt", 0.5)
-    plot_actor_frequencies("exp_results_t_0.6_m_hog_u_1.txt", 0.6)
-    plot_actor_frequencies("exp_results_t_0.7_m_hog_u_1.txt", 0.7)
+    # plot_actor_frequencies("exp_results_t_0.5_m_hog_u_1.txt", 0.5)
+    # plot_actor_frequencies("exp_results_t_0.6_m_hog_u_1.txt", 0.6)
+    # plot_actor_frequencies("exp_results_t_0.7_m_hog_u_1.txt", 0.7)
 
-    #plot_actor_frequencies("exp_results_t_0.5_m_cnn_u_1.txt",0.6)
-    #plot_actor_frequencies("exp_results_t_0.6_m_cnn_u_1.txt",0.7)
+    # plot_actor_frequencies("exp_results_t_0.5_m_cnn_u_1.txt",0.6)
+    # plot_actor_frequencies("exp_results_t_0.6_m_cnn_u_1.txt",0.7)
 
-    plot_actor_frequencies("exp_results_manual.txt", "gt", multiplier=4.059)
+    # plot_actor_frequencies("exp_results_manual.txt", "gt", multiplier=4.059)
+    plot_video_frames("exp_results_t_0.6_m_hog_u_1.txt", "0.6 100% sample")
+    plot_video_frames("exp_results_manual.txt", "ground truth 25% sample")
+
