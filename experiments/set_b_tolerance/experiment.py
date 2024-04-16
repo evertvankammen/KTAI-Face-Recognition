7a7fd0b07b7785855396ffee07839d118cf2f6b8
evert <evert.vankammen@qfactors.nl>
2024-04-16 13:29:27 +0200

    Add graph functions and update experiment script

--- experiments/set_b_tolerance/experiment.py
+++ experiments/set_b_tolerance/experiment.py
@@ -8,9 +8,9 @@ def calculate_mse(c1: Counter, c1frames: int, c2: Counter, c2frames: int):
     return mse
 
 
-def addlabels(x, y):
+def addlabels(x, y , offset=0.3):
     for i in range(len(x)):
-        plt.text(i, y[i], round(y[i]))
+        plt.text(i + offset , y[i], round(y[i]))
 
 
 def plot_actor_frequencies(file_path, tolerance, multiplier=1.0):
@@ -41,6 +41,57 @@ def plot_actor_frequencies(file_path, tolerance, multiplier=1.0):
     plt.show()
 
 
+def extract_counter_from_file(file_path):
+    """Extract the Counter from a text file."""
+    # Initialiseer een lege Counter
+    actors = []
+    with open(file_path, 'r') as fp:
+        lines = fp.readlines()
+        for line in lines[4:]:
+            actor, frame = line.split()
+            actors.append(actor)
+
+    return Counter(actors)
+
+
+ground_truth = Counter(
+    {'Chandler': 1324, 'Joey': 1168, 'Monica': 2556, 'Phoebe': 524, 'Rachel': 1156, 'Ross': 1824, 'Unknown': 2756})
+
+
+def compare_counters(file_path, ground_truth_path, extra_text):
+    """Compare two Counters and plot the comparison."""
+    # Combineer de acteurs uit beide Counters
+    experiment = extract_counter_from_file(file_path)
+    ground_truth = extract_counter_from_file(ground_truth_path)
+
+    actors = sorted(set(experiment.keys()) | set(ground_truth.keys()))
+
+    # Haal de aantallen frames op voor elke acteur
+    frames_counter1 = [experiment.get(actor, 0) for actor in actors]
+    frames_counter2 = [4 * ground_truth.get(actor, 0) for actor in actors]
+
+    # Plot de vergelijking
+    plt.figure(figsize=(10, 6))
+    x = range(len(actors))
+    plt.bar(x, frames_counter1, width=0.4, label='Extracted Counter', color='blue',zorder=3)
+    addlabels(x, frames_counter1, -0.15)
+    x2 = [i + 0.4 for i in x]
+    plt.bar(x2, frames_counter2, width=0.4, label='Ground Truth Counter', color='orange',zorder=3)
+    addlabels(x2, frames_counter2, +0.25)
+    plt.xlabel('Actors')
+    plt.ylabel('Number of Frames')
+    plt.ylim([0, 5500])
+    plt.title(f'Comparison of Extracted Counter {extra_text} vs Ground Truth Counter')
+    plt.xticks([i + 0.2 for i in x], actors)
+    plt.legend()
+    plt.grid(axis='y',zorder=0)
+    plt.tight_layout()
+
+    store_experiment = f"_upsample_{extra_text}.png"
+    plt.savefig(store_experiment)  # save the figure to file
+    plt.show()
+
+
 def get_needed_frames():
     frames = []
     with open("exp_results_manual.txt", 'r') as fp:
@@ -63,8 +114,6 @@ def plot_video_frames(file_path, tolerance):
             actor, frame = line.split()
             if int(frame) in frms:
                 actor_frames[actor].append(int(frame))
-
-
 
     s = sorted(actor_frames.items())
 
@@ -110,7 +159,14 @@ if __name__ == '__main__':
     # plot_actor_frequencies("exp_results_t_0.5_m_cnn_u_1.txt",0.6)
     # plot_actor_frequencies("exp_results_t_0.6_m_cnn_u_1.txt",0.7)
 
-    # plot_actor_frequencies("exp_results_manual.txt", "estimated ground truth", multiplier=4.059)
+    # plot_actor_frequencies("exp_results_manual.txt", "estimated ground truth", multiplier=4)
+    plot_video_frames("exp_results_t_0.5_m_hog_u_1.txt", "0.5 same as ground truth")
     plot_video_frames("exp_results_t_0.6_m_hog_u_1.txt", "0.6 same as ground truth")
+    plot_video_frames("exp_results_t_0.7_m_hog_u_1.txt", "0.7 same as ground truth")
+
+
     # plot_video_frames("exp_results_manual.txt", "ground truth 25% sample")
 
+    # compare_counters("exp_results_t_0.5_m_hog_u_1.txt", "exp_results_manual.txt", "0.5 tolerance")
+    # compare_counters("exp_results_t_0.6_m_hog_u_1.txt", "exp_results_manual.txt", "0.6 tolerance")
+    # compare_counters("exp_results_t_0.7_m_hog_u_1.txt", "exp_results_manual.txt", "0.7 tolerance")
